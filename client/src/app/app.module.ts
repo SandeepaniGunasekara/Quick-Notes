50ec31be0c83ed607bd57267aa1f2fed38411858
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2023-05-07 02:40:02 +0530

    created header, new note and saved notes components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppRoutingModule} from "./app-routing.module";
+import { HeaderComponent } from './components/header/header.component';
+import { NewNoteComponent } from './components/new-note/new-note.component';
+import { SavedNotesComponent } from './components/saved-notes/saved-notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    NewNoteComponent,
+    SavedNotesComponent,
   ],
   imports: [
     BrowserModule,
